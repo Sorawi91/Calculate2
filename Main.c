21701e25679c78b3078e907ff7f017e7c4d537c8
Wut <talonpopy@gmail.com>
2026-02-02 13:08:13 +0700

    add fix matermatic logic

--- Main.c
+++ Main.c
@@ -1,64 +1,52 @@
 #include <stdio.h>
 
-int Persen (int a, int b) {
-    a % b;
-    return a % b;
-} //minny
-
-int divide (int a, int b) { 
-return (b != 0) ? a / b : 0; 
+int Persen(int a, int b) {
+    return (b != 0) ? a % b : 0;
 }
 
-int multiply (int a, int b) {
+int divide(int a, int b) {
+    return (b != 0) ? a / b : 0;
+}
+
+int multiply(int a, int b) {
     return a * b;
 }
 
-int minus (int a, int b) {
-    a - b ;
+int minus(int a, int b) {
     return a - b;
-} //Guitar
+}
 
-int plus (int a,int b) {
-    a + b;
+int plus(int a, int b) {
     return a + b;
-} //Kafe
+}
 
-int calculate(int a,char op,int b) {
-    int total;
-    if (op == '+') {
-        total = plus(a,b);
-    } else if (op == '-') {
-        total = minus(a,b);
-    } else if (op == '*') {
-        total = multiply(a,b);
-    } else if (op == '/') {
-        total = divide(a,b);}
-     else {
-        total = Persen(a,b);
-    }
+int calculate(int a, char op, int b) {
+    int total = a;
 
-    //ใครทำส่วนไหนเเล้วก็ให้เอาเครื่องหมายคอมเม้นออกในส่วนของตัวเองไม่งั้นโค้ดรันไม่ได้
+    if (op == '+') total = plus(a, b);
+    else if (op == '-') total = minus(a, b);
+    else if (op == '*') total = multiply(a, b);
+    else if (op == '/') total = divide(a, b);
+    else if (op == '%') total = Persen(a, b);
 
-    printf("Process: %d %c %d = %d\n",a,op,b,total);
+    printf("Process: %d %c %d = %d\n", a, op, b, total);
     return total;
 }
 
-int main () {
-    int first , next;
+int main() {
+    int first, next;
     char op;
 
-    if (scanf("%d",&first) != 1) {
-        return 0;
-    } //มีไว้เช็คเเค่ตัวอักษรที่ไม่ใช่ตัวเลข
+    if (scanf("%d", &first) != 1) return 0;
 
-    while (scanf(" %c %d",&op,&next) == 2) {
-        first = calculate(first,op,next); //ในลูปรอบเเรกที่มันรูปจะเอา first กับ เครื่องหมายไปเเล้ว next เข้าไปเช็คในฟังชั่นเเละฟังชั่นจะรีเทรินค่าออกมาเป็น first เเละเก็บค่า first นำไปเข้าฟังชั่นจนจบสมการที่เขียนไป
-    } //มันจะลูปรับค่าไอ้ op เเละ next ไปเรื่อยๆจนกว่าจะไม่ใช่ตัวเลขมันก็จะหยุด เเละใน ลูปมันคือการนำตัวเลขที่กรอกไปพร้อมกับเครื่องหมายไปเข้าฟังชั่นคำนวน เเละเเสดงโปรเซสที่มันคิดออกมา
+    while (1) {
+        scanf(" %c", &op);
+        if (op == '=') break;
 
+        scanf("%d", &next);
+        first = calculate(first, op, next);
+    }
 
-    printf("Anwer: %d\n",first);
-
-    //ให้พิมพ์ cls เพื่อออกจาการทำงาน
-
+    printf("Answer: %d\n", first);
     return 0;
 }
